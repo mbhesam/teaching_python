b68a4c263768c8ad0e7258565cd9609fdf253585
Mohammad Bagher Hesam <88931611+mbhesam@users.noreply.github.com>
2021-11-07 14:15:57 +0330

    Add files via upload

---
+++ mohasebe_ebarat/solution.py
@@ -0,0 +1,8 @@
+x = input("assign value to x with 1 decimal place")
+x = float(x)
+y = input("assign value to y with 1 decimal place")
+y = float(y)
+soorat = x + 1
+makhraj = x+((y**2)*5)
+answer = soorat/makhraj
+print("the answer of question is:\t"+str(answer))
