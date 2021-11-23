7968af7add81426ff50c77fb650569787bf61ba1
Mohammad Bagher Hesam <88931611+mbhesam@users.noreply.github.com>
2021-11-23 14:14:18 +0330

    Add files via upload

---
+++ palindrom/polindrom.py
@@ -0,0 +1,18 @@
+word = input()
+reveredword=word[::-1]
+if reveredword == word :
+    print("polindrom")
+else:
+    print("not polindrom")
+   
+
+*******************************************************************
+word = input()
+w_l = list(word)
+w_l.reverse()
+reversedword = "".join(w_l)
+if reveredword == word :
+    print("polindrom")
+else:
+    print("not polindrom")
+   
